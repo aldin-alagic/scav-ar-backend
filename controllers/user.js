fb5ba2a20472e660b3b3faf9273b0a48d7905847
Aldin <aldin.alagicc@gmail.com>
2021-09-27 22:50:53 +0200

    Change user register error message

--- controllers/user.js
+++ controllers/user.js
@@ -38,7 +38,7 @@ export const register = async (req, res) => {
 
     res.status(200).json({ success: true, message: "You have been successfully registered!" });
   } catch (error) {
-    res.status(400).json({ success: false, message: "Unable to register user! Please check your input." });
+    res.status(400).json({ success: false, message: "Unable to register user! Error: " + error });
   }
 };
 
